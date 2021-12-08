d9a2430fe1c0d305b800433d917efa5991e240de
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 16:06:30 -0600

    video with notes

--- practice/main.go
+++ practice/main.go
@@ -8,6 +8,19 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	//dow := rand.Intn(7) + 1
+	//fmt.Println("Day", dow)
+	//fallthrough can be used to continue through the case,otherwise it will exit after one is met.
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "It's Sunday!"
+		//fallthrough
+	case 2:
+		result = "It's Monday!"
+		//fallthrough
+	default:
+		result = "It's some other day!"
+	}
+	fmt.Println(result)
 }
