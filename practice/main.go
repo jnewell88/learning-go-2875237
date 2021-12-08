b6ea32f347ca2a678465f43a4b371d85b0206c5c
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 15:39:31 -0600

    following video with notes

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+
+	theAnswer := 42
+	var result string
+
+	//Standard if/else if/else statement
+	if theAnswer < 0 {
+		result = "Less than zero"
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
+	//Can have variable initialization as part of the if statement
+	if x := -42; x < 0 {
+		result = "Less than zero"
+	} else if x == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
 }
