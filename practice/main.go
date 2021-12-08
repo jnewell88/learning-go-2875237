393756b9dd8176e170fa0ee189e64540bb8096dc
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 15:32:29 -0600

    missed file

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	//Classes/structs, data and methods
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.Weight = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+//Custom type
+//lowercase = non exported (private), uppercase = exported (public)
+type Dog struct {
+	Breed  string
+	Weight int
 }
