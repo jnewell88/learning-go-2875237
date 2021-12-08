a66a143eb49c66fb9ee9e92f9c7b7a55d46c4fed
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 15:00:14 -0600

    work in video, with notes

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	//& = memory address
+	//* = memory value
+	var p = &anInt
+	fmt.Println("Value of p:", *p)
+
+	value1 := 42.13
+	pointer1 := &value1
+	fmt.Println("Value 1:", *pointer1)
+
+	//Setting pointer to a new value, which changes value1
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Pointer 1:", *pointer1)
+	fmt.Println("Value 1:", value1)
 }
