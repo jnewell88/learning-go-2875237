a2cf451169900937fecb8f0044343a16b2263b52
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 15:21:19 -0600

    video with notes

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+
+	//Map is unordered list of key value pairs (Hash)
+	states := make(map[string]string)
+	fmt.Println(states)
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+
+	delete(states, "OR")
+	fmt.Println(states)
+
+	states["NY"] = "New York"
+	fmt.Println(states)
+
+	//iterate through map
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	//List in alphabetical order
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	//This list is sortable, so sort it to get alphabetical
+	fmt.Println(keys)
+	sort.Strings(keys)
+	fmt.Println(keys)
+
+	//Print out result of states (with the ordered key indexes ordering the list)
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 }
