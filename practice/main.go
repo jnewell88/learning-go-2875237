aa0739c67be1d82862b7b26f9da72c6f79533988
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-08 15:04:22 -0600

    video following

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	//Array init
+	var colors [3]string
+	//Setting values
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers", len(numbers))
 }
