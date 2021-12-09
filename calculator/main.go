513164e482608296d4455637005579a923270b0e
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-09 10:29:45 -0600

    follow video with notes

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "Hello from Go!"
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	//Defer: wait until everything else has executed, then run this
+	defer file.Close()
+	//File needs to be completely closed before trying to read
+	defer readFile("./fromString.txt")
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file:", string(data))
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
