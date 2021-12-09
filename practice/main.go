3fdeb192bc5782e9755402de0082b040ae52332f
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-09 10:49:56 -0600

    follow video, notes added

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -25,5 +27,37 @@ func main() {
 	}
 
 	content := string(bytes)
-	fmt.Print(content)
+	//Raw JSON output print
+	//fmt.Print(content)
+
+	tours := toursFromJson(content)
+	for _, tour := range tours {
+		fmt.Printf("Tour: %v, Price: $%v\n", tour.Name, tour.Price)
+	}
+}
+
+//slice of tours
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	//decoder.More reads the next available object from json object (if one found True, otherwise False)
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour)
+	}
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
 }
