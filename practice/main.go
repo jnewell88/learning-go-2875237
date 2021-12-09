102abaf71e374d24ffb538a93e308a4eb80297e7
jnewell-ms <jnewell@spideroak-ms.com>
2021-12-09 09:34:09 -0600

    video plus comments

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	sum := addValues(5, 8)
+	fmt.Println("The sum is:", sum)
+	multiSum, multiCount := addAllValues(4, 7, 9, 11)
+	fmt.Println("Sum of multiple values:", multiSum)
+	fmt.Println("Count of items", multiCount)
+}
+
+//Not returning anthing, don't need a return type beween () and {
+
+func doSomething() {
+	fmt.Println("Doing something")
+}
+
+func addValues(value1 int, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
